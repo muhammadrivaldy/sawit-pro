002981c71bdf44b89c36b961a511490e8d96bd7c
Rivaldy SBS <muhammad.rivaldy@flip.id>
2023-12-09 21:15:23 +0700

    Create table mst_users and create a docker-compose for running the dependencies
    On branch develop
    Your branch is up to date with 'origin/develop'.
    Changes to be committed:
    	new file:   docker-compose.yaml
    	modified:   services/users/entities/databases/migrations/20231209132649_init.down.sql
    	modified:   services/users/entities/databases/migrations/20231209132649_init.up.sql

---
+++ services/users/entities/databases/migrations/20231209132649_init.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE mst_users (
+    id serial8 PRIMARY KEY,
+    full_name varchar(60) NOT NULL,
+    phone_number varchar(15) NOT NULL,
+    password_hash varchar NOT NULL,
+    created_by int8 default 0 NOT NULL,
+    created_at timestamptz NOT NULL,
+    updated_by int8,
+    updated_at timestamptz
+);
