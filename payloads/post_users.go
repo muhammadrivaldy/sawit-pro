a6785e389992056084b3ce6fe906769f9de23aa1
Rivaldy SBS <muhammadrivaldy16@gmail.com>
2023-12-10 14:42:17 +0700

    Implementing password validation

--- payloads/post_users.go
+++ payloads/post_users.go
@@ -3,7 +3,7 @@ package payloads
 type RequestPostUsers struct {
 	PhoneNumber string `json:"phone_number" validate:"required,min=12,max=15"`
 	FullName    string `json:"full_name" validate:"required,min=3,max=60"`
-	Password    string `json:"password" validate:"required,min=6,max=64"`
+	Password    string `json:"password" validate:"required,password,min=6,max=64"`
 }
 
 type ResponsePostUsers struct {
